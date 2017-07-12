0a5706561987e7450b02a6949a889738ff996005
Hannah <hannahlspencer@gmail.com>
2017-07-12 22:37:36 +0100

    Create noughtsAndCrosses.java
    Main method for game

---
+++ src/main/java/noughtsAndCrosses.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class noughtsAndCrosses {
+  
+
+
+  public static void main(String[] args) {
+	
+	  System.out.println("");
+    System.out.println("                Welcome to Noughts and Crosses");
+	  System.out.println("                -------------------------------");
+	  System.out.println("Who will play first? Noughts (o) or crosses (x)?");
+    XOUtils.toStart();
+	  XOUtils.makeChoice();
+  }
+
+}
